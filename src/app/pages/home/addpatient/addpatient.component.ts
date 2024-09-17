21527059cabdf565360f460452c365d239903262
shubhamsinghvsc <shubham.s@vsoftwareconsulting.com>
2024-09-17 17:41:43 +0530

    updated leave and patient

--- src/app/pages/home/addpatient/addpatient.component.ts
+++ src/app/pages/home/addpatient/addpatient.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { CommunicationService } from 'src/app/services/communication.service';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-addpatient',
@@ -8,11 +11,44 @@ import { NzNotificationService } from 'ng-zorro-antd/notification';
 })
 export class AddpatientComponent {
 
+  constructor(private _CS: CommunicationService, private datePipe: DatePipe) { }
+
+  PatientID: any;
+  Name: string;
+  DOB: any;
+  ContactNumber: string;
+  Email: string;
+  selectedGender: number;
+
+  addPatient(): void {
+    this.DOB = this.datePipe.transform(this.DOB, 'yyyy-MM-dd');
+
+    const patient = {
+      patientId: 0,
+      name: this.Name,
+      dateOfBirth: this.DOB,
+      gender: Number(this.selectedGender),
+      contactNumber: this.ContactNumber,
+      email: this.Email
+    }
+
+    this._CS.AddPatient(patient).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.PatientID = response.patientId;
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+
+    });
+  }
   // constructor(private notification: NzNotificationService) {
-    
+
   // }
-    
-    
+
+
   // createBasicNotification(): void {
   //   this.notification
   //     .blank(
@@ -23,6 +59,6 @@ export class AddpatientComponent {
   //       console.log('notification clicked!');
   //     });
   // }  
-    
-    
+
+
 }
--- src/app/pages/home/leaves/leaves.component.ts
+++ src/app/pages/home/leaves/leaves.component.ts
@@ -1,12 +1,14 @@
+import { DatePipe, formatDate, Time } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommunicationService } from 'src/app/services/communication.service';
 
 interface Appointment {
   AppointmentID: string;
   DoctorName: string;
   PatientName: string;
   AppointmentDate: Date;
-  AppointmentTime:Date;
-  AppointmentStatus:string;
+  AppointmentTime: Date;
+  AppointmentStatus: string;
 }
 @Component({
   selector: 'app-leaves',
@@ -18,38 +20,81 @@ interface Appointment {
 
 export class LeavesComponent {
 
+  constructor(private _CS: CommunicationService, private datePipe: DatePipe) {
+    this.updateTable();
+  }
+
+
+  listOfLeave: any;
+  dcotorId: number = 1;  // for now it is hardcoded later we have to align with login in docterid.
+
+  startDate: Date;
+  endDate: Date;
+  startTime: string = "0000-00-00T00:00:00";
+  endTime: string = "0000-00-00T00:00:00";
+  leaveType: number;
+  description: string;
+  dateRange: Date[] = [];
+
+  // Tue Sep 17 2024 00:00:33 GMT+0530 (India Standard Time)
+
   time: Date | null = null;
   size: 'large' | 'small' | 'default' = 'default';
   log(value: Date): void {
     console.log(value);
   }
 
+  addLeave(): void {
+    console.log("Start Timee : " + this.startTime);
 
-  NowDate:Date=new Date();
-  listOfData: Appointment[] = [
-    {
-      AppointmentID: "AID001",
-      DoctorName: "Dr.Priya ",
-      PatientName: "Patient One",
-      AppointmentDate:this.NowDate ,
-      AppointmentTime:this.NowDate,
-      AppointmentStatus:"Scheduled"
-    },
-    {
-      AppointmentID: "AID001",
-      DoctorName: "Dr.Priya ",
-      PatientName: "Patient One",
-      AppointmentDate: this.NowDate,
-      AppointmentTime:this.NowDate,
-      AppointmentStatus:"Scheduled"
-    },
-    {
-      AppointmentID: "AID001",
-      DoctorName: "Dr.Priya ",
-      PatientName: "Patient One",
-      AppointmentDate: this.NowDate,
-      AppointmentTime:this.NowDate,
-      AppointmentStatus:"Scheduled"
+    // Format dates for backend
+    const formattedStartDate = this.datePipe.transform(this.startDate, 'yyyy-MM-dd');
+    const formattedEndDate = this.datePipe.transform(this.endDate, 'yyyy-MM-dd');
+    const formattedStartTime = this.datePipe.transform(this.startTime, 'HH:mm:ss');
+    const formattedEndTime = this.datePipe.transform(this.endTime, 'HH:mm:ss');
+
+    console.log("dateee : " + formattedStartTime);
+
+    const leave = {
+      leaveId: 0,
+      doctorId: 1,
+      startDate: formattedStartDate, //"2024-09-17T06:36:13.041Z", //this.startDate,
+      endDate: formattedEndDate, //"2024-09-17T06:36:13.041Z", // this.endDate,
+      startTime: formattedStartTime, //"08:00:00",// this.startTime,
+      endTime: formattedEndTime, //"08:00:00", //this.endTime,
+      leaveType: Number(this.leaveType),
+      description: this.description
     }
-  ];
+
+    this._CS.AddLeave(leave).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+    this.updateTable();
+
+  }
+
+  updateTable(): void {
+    this._CS.GetDoctorLeaveById(this.dcotorId).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.listOfLeave = response;
+      },
+      error: (error) => {
+        console.log(error);
+
+      }
+    });
+  }
+
+  onDateChange(result: Date[]): void {
+    if (result && result.length == 2) {
+      this.startDate = result[0];
+      this.endDate = result[1];
+    }
+  }
 }
